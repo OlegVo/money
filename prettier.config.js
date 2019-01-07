580f17ca23ba6a482cd25b69418d8778a6e4ac6d
Oleg Vorobyov <oleg.vorobiov@gmail.com>
2019-01-08 02:26:11 +0700

    Yarn и prettier

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+// https://prettier.io/docs/en/options.html
+module.exports = {
+  useTabs: false, // Использовать пробелы в качестве табуляции
+  tabWidth: 4, // Ширина отступов
+  semi: true, // Ставить ; в конце каждого выражения
+  singleQuote: true, // Одинарные кавычки вокруг строк
+  jsxSingleQuote: true,
+  trailingComma: 'all', // Выставлять запятые в конце массивов, объектов, аргументов функций и пр.
+  bracketSpacing: true, // Добавлять пробелы перед/после скобок, Example: { foo: bar }
+  jsxBracketSameLine: false,
+
+  // proseWrap: 'preserve',
+  printWidth: 140,
+};
