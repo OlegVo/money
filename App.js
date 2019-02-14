4cf6a4a88feed07a237025b7402329bb92133171
Oleg <oleg.vorobiov@gmail.com>
2019-02-14 14:47:06 +0700

    Завёл на Expo
    Поставил Expo и запустил на телефоне!

---
+++ App.js
@@ -0,0 +1,3 @@
+import App from './build/app/containers/App';
+
+export default App;
--- index.ios.js
+++
@@ -1,5 +0,0 @@
-import { AppRegistry } from 'react-native';
-
-import App from './build/app/containers/App';
-
-AppRegistry.registerComponent('money', () => App);
